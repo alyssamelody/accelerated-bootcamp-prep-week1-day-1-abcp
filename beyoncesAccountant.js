ab2fec2fb0628982c6e38bf5799ba7fd2b0e37a0
Alyssa Melody Paglumotan <lissamelodia@gmail.com>
2018-01-21 19:11:10 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,9 +1,10 @@
 var name = "Beyonce"; 
 var total_sum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000;
+var beyoncesMoney = total_sum;
 
 console.log("Dear " + name)
 console.log("We have calculated your total earnings this year.")
-console.log("You have earned " + total_sum)
+console.log("You have earned " + beyoncesMoney)
 console.log("Of some portion derives from your live performances.")
 console.log("Of" + total_sum + " some portion derives from music sales.")
 
